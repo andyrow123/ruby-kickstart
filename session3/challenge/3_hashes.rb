70dda4657f9b58803aac798e1db430eb8dd1b022
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-09-06 19:44:01 +0100

    Session 3, challenge 3 completed

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -7,5 +7,7 @@
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
 def word_count(string)
-
+  Hash.new(0).tap{ |hash| string.downcase.split(' ').each { |word| hash[word] += 1}}
 end
+
+word_count "The dog and the cat"
