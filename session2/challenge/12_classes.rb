e783c8abe181851ae006bff414a4906217eca7bf
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-09-06 10:55:34 +0100

    Session 2, challenge 12 completed

--- session2/challenge/12_classes.rb
+++ session2/challenge/12_classes.rb
@@ -21,8 +21,38 @@
 # f.to_f               # => 0.5
 
 class Fraction
+  attr_accessor :numerator, :denominator
+
+  def initialize(numerator, denominator)
+    @numerator = numerator
+    @denominator = denominator
+  end
+
+  def to_s
+    "#{@numerator}/#{@denominator}"
+  end
+
+  def to_f
+    @numerator.to_f / @denominator.to_f
+  end
+
+  def lowest
+    cd = gcd(@numerator, @denominator)
+    Fraction.new(@numerator / cd, @denominator / cd)
+  end
+
   def gcd(a,b)
     return a if b == 0
     gcd(b, a%b)
   end
 end
+
+f = Fraction.new 20, 60
+f.numerator          # => 20
+f.denominator        # => 60
+f.to_s               # => "20/60"
+f.lowest.to_s        # => "1/3"
+f.numerator = 50
+f.denominator = 100
+f.to_s               # => "50/100"
+f.to_f               # => 0.5
