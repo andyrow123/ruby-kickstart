fecc34856718a30d7ce0f14fc83dfc68e3418e7d
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-09-08 08:39:16 +0100

    Session 3, challenge 8 completed

--- session3/challenge/8_blocks.rb
+++ session3/challenge/8_blocks.rb
@@ -23,17 +23,36 @@
 #
 # artist.name   # => "The Artist Formarly Known As Prince"
 # artist.age    # => 47
-
+require 'date'
 
 class Person
-  attr_accessor :name
+  attr_accessor :name, :age, :birthday, :quote
 
-  def initialize(&initializer)
-    @initializer = initializer
-    initializer.call self
+  def initialize(options={}, &initializer)
+    self.name = options[:name]
+    self.age  = options[:age]
+    self.birthday = options[:birthday]
+    self.quote  = options[:quote]
+
+    # this way, it always has a proc, and code like reinit doesn't have to worry that it might not be there
+    @initializer = (initializer || Proc.new {|person|})
+
+    reinit
   end
 
   def reinit
-    @initializer.call self
+    @initializer.call(self)
   end
 end
+
+austin = Person.new :name => 'Austin', :birthday => Date.parse('1979/09/16'), :age => 37 do |person|
+  # person.name = 'Austin'
+  # person.age = 37
+  # person.birthday = Date.parse('1979/09/16')
+  person.quote = "Blah blah blah!"
+end
+
+puts austin.name
+puts austin.age
+puts austin.birthday
+puts austin.quote
