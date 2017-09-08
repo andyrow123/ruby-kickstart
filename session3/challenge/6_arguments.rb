b9b9eddb3be67624694f76174938735a9158b657
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-09-08 06:09:40 +0100

    Session 3, challenge 6 completed

--- session3/challenge/6_arguments.rb
+++ session3/challenge/6_arguments.rb
@@ -17,3 +17,11 @@
 # match_maker true,  true,  true, true, nil     # => [false, true]
 # match_maker true,  true,  true, 0, nil        # => [false, true]
 
+def match_maker(decider, *elements)
+  Array.new.tap do |item|
+    elements.each_slice 2 do |left, right|
+      left, right = !!left, !!right
+      item << (decider ? left != right : left == right)
+    end
+  end
+end
